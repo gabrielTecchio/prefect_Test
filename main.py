900d525e6839a8b7b1d8adcdca47c1999aaea83d
Gabriel Glanert Tecchio <gabrieltecchio@hotmail.com>
2024-11-01 17:32:35 -0300

    New Commit

--- main.py
+++ main.py
@@ -1,71 +1,50 @@
-import requests
-import pandas_gbq
+# -*- coding: UTF-8 -*-
 import pandas as pd
-from prefect import flow, task
-import os
+from datetime import datetime
+from google.oauth2 import service_account
+from pandas_gbq import to_gbq
+from prefect import flow, task, get_run_logger
 
-os.environ["PYTHONUTF8"] = "1"  # Forces UTF-8 encoding in subprocesses.
-
+# Function to create data to BigQuery
 @task
-def requestReponses(url: float) -> list:
-    # Make the GET request
-    response = requests.get(url)
+def create_msg(msg: str = "This is a log msg from function create_msg!") -> pd.DataFrame:
+    print("Creating msg!")
+    # Create a DataFrame with the current date, time, and message
+    data = {
+        "date": [datetime.now().date()],
+        "time": [datetime.now().time()],
+        "message": [msg]
+    }
+    return pd.DataFrame(data)
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-        # print(data)
-        return pd.DataFrame(data)
-    else:
-        print(f"Request failed with status code {response.status_code}")
-        return None
-
+# Function to write data to BigQuery
 @task
-def sendToBQ(dataFrame: pd.core.frame.DataFrame, projID:str, dados:str, tabela:str) -> str:
-    pandas_gbq.to_gbq(dataFrame, f"{projID}.{dados}.{tabela}", if_exists="replace", project_id=projID)
+def send_to_gbq(df, PROJECT_ID, DATASET_NAME, TABLE_NAME, cred):
+    print("Send data to GBQ!")
+    # Define the full table ID
+    table_id = f"{DATASET_NAME}.{TABLE_NAME}"
+
+    # Write the data to BigQuery, replacing the table if it doesn't exist
+    to_gbq(df, table_id, project_id=PROJECT_ID, if_exists="append", credentials=cred)
+    return("Data logged to BigQuery successfully.")
 
 @flow(log_prints=True)
-def flowBCBtoGBQ(codigo_serie):
-    #codigo_serie = 21774 # 11 = SELIC; 1207 = PIB; 21774 = População
-    #codigo_serie = None
-    # while not codigo_serie:
-        #codigo_serie: int = int(input("Enter a number (11 = SELIC; 1207 = PIB; 21774 = População; 11426 = IPCA): "))
-    match int(codigo_serie):
-            case 11:
-                print("SELIC")
-                tabela = "tabela-selic-teste"
-            case 1207:
-                print("PIB")
-                tabela = "tabela-pib-teste"
-            case 21774:
-                print("População")
-                tabela = "tabela-populacao-teste"
-            case 11426:
-                print("IPCA")
-                tabela = "tabela-ipca-teste"
-            case 1:
-                print("Exchange Rate (US Dollar)")
-                tabela = "tabela-dolar-teste"
-            case _:
-                codigo_serie = None
+def myFlow():
+    # Define your Google Cloud project ID and BigQuery dataset and table names
+    PROJECT_ID = "treinamentos-420711"  # Replace with your Google Cloud project ID
+    DATASET_NAME = "tabela_selic"  # Replace with your BigQuery dataset name
+    TABLE_NAME = "test_log"  # Replace with your desired table name
 
-    project_id = "treinamentos-420711"
-    conj_dados = "tabela_selic"
+    # Path to your Google Cloud service account key file
+    SERVICE_ACCOUNT_FILE = "key.json"  # Replace with the path to your JSON key file
 
-    # Define the API endpoint
-    url = f"https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo_serie}/dados?formato=json"
+    # Authenticate using the service account file
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE)
 
-    df = requestReponses(url)
-    print(type(df))
-    sendToBQ(df, project_id, conj_dados, tabela)
-    return "Succes"
+    df = create_msg("This is a log msg from myFlow!")
+    finalResult = send_to_gbq(df, PROJECT_ID, DATASET_NAME, TABLE_NAME, credentials)
+    print(finalResult)
 
+# Run the function
 if __name__ == "__main__":
-    # Enter a number (11 = SELIC; 1207 = PIB; 21774 = População; 11426 = IPCA)
-    flowBCBtoGBQ.serve(
-        name="my-first-deployment",
-        tags=["onboarding"],
-        parameters={"codigo_serie": 1},
-        interval=60
-    )
+    myFlow()
